2977794af3058d12a58cdda3e8c4256f6dba8414
AtmaWeapon <ajdittli@gmail.com>
2026-01-08 22:01:33 -0700

    in progress: blog encryptor widget

--- neocity/coding/encryptor.js
+++ neocity/coding/encryptor.js
@@ -1,7 +1,8 @@
-const plaintext = getElementById("blog-content-input");
-const key = getElementById("encryption-key-input");
-const output = getElementById("encrypted-blog-content");
 
 function encrypt() {
+  const plaintext = document.getElementById("blog-content-input");
+  const key = document.getElementById("encryption-key-input");
+  const output = document.getElementById("encrypted-blog-content");
+
   output.innerHTML = encipher(plaintext.value, key.value);
 }
--- neocity/helpers/cipher.js
+++ neocity/helpers/cipher.js
@@ -1,12 +1,15 @@
 const tagRegex = /\<[^\>]\>/g;
-const asciiInterval = [32, 126] // TODO: exclude certain characters from this range
-const charCount = asciiInterval[1] - asciiInterval[0] + 1
+const asciiInterval = [32, 126] // TODO: exclude certain characters from this range, probably "'", '"', "<", ">", "&"
+const charCount = asciiInterval[1] - asciiInterval[0] + 1 // 95
 
 function encipher(plaintext, key) {
-  if (!key || !plaintext) return;
+  console.log("plaintext", plaintext);
+  console.log("key", key);
+
+  if (!key || !plaintext) return plaintext;
 
   var matches = []
-  while ((match = regex.exec(plaintext)) != null) {
+  while ((match = tagRegex.exec(plaintext)) != null) {
     console.log("match found at " + match.index);
     matches.push(match)
   }
@@ -24,8 +27,12 @@ function encipher(plaintext, key) {
         charCodes.push(tag[0].charAt(c).charCodeAt());
       }
     } else {
+      const output = actualMod((plaintext.charCodeAt(i) - key.charCodeAt(i % key.length)) - 32, charCount) + 32;
+      console.log(plaintext.charCodeAt(i));
+      console.log(key.charCodeAt(i % key.length));
+      console.log(output);
+      charCodes.push(output);
       i++;
-      charCodes.push(plaintext.charCodeAt(i) - key.charCodeAt(i % key.length)); // TODO: mod output to within the asciiInterval
     }
   }
   console.log("CHAR CODES:", charCodes);
@@ -37,4 +44,8 @@ function encipher(plaintext, key) {
 
 function decipher(ciphertext, key) {
 
+}
+
+function actualMod(input, mod) {
+  return (input%mod+mod)%mod
 }
