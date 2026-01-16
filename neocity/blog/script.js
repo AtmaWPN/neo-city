ce3401b4968f34d57a02a4560db8f7d7b376167c
Albin Dittli <ajdittli@gmail.com>
2026-01-16 10:33:27 -0700

    hide encrypted posts by default, address posts by date

--- neocity/blog/script.js
+++ neocity/blog/script.js
@@ -12,37 +12,64 @@ const postsJson = fetch(`${window.location.origin}/blog/posts.json`)
   .then((response) => response.json())
   .then((json) => {
     const queryParams = new URLSearchParams(window.location.search);
-    const postParam = queryParams.get("post");
-    const blogPost = postParam !== null && !isNaN(parseInt(postParam)) ? parseInt(postParam) : json.posts.length - 1;
-    const postIndex = json.posts.length - 1 - blogPost;
+    const showEncryptedParam = queryParams.get("e");
+    const postParam = queryParams.get("post")?.split("-");
+    const [year, month, day] = postParam?.length === 3 ? postParam.map((it) => parseInt(it)) : [null, null, null];
+
+    let posts = json.posts;
+    if (showEncryptedParam !== "true") {
+      posts = json.posts.filter((it) => !it.encrypted)
+      if (posts.every((it) => it.year !== year || it.month !== month || it.day !== day)) {
+        const actualPost = json.posts.find((it) => it.year === year && it.month === month && it.day === day);
+        if (actualPost) {
+          posts.push(actualPost);
+        }
+      }
+    }
+    posts.sort((a, b) => a.day - b.day)
+      .sort((a, b) => a.month - b.month)
+      .sort((a, b) => a.year - b.year);
+
+    const postIndex = postParam?.length === 3 ? posts.findIndex((it) => it.year === year && it.month === month && it.day === day) : posts.length - 1;
+    if (postIndex === -1 || postIndex === null || postIndex === undefined) {
+      decryptor.style.display = "none";
+      content.innerHTML = `<p style="color:#f14c4c;">Invalid Date Error: ${
+        !Number.isNaN(year) && !Number.isNaN(month) && !Number.isNaN(day)
+          ? `There is no post from ${month}/${day}/${year}`
+          : `${postParam.join("-")} is not a valid date`
+      }</p><p>Be sure to format dates yyyy-mm-dd</p>`;
+      return json;
+    }
     
-    prev.href = `${window.location.origin}/blog.html?post=${Math.max(blogPost - 1, 0)}`;
-    next.href = `${window.location.origin}/blog.html${blogPost >= json.posts.length - 1 ? "" : `?post=${blogPost + 1}`}`;
+    const prevPost = posts[postIndex - 1] ?? posts[0];
+    prev.href = `${window.location.origin}/blog.html?post=${prevPost.year}-${prevPost.month}-${prevPost.day}${showEncryptedParam === "true" ? "&e=true" : ""}`;
+    const nextPost = posts[postIndex + 1] ?? posts[posts.length - 1];
+    next.href = `${window.location.origin}/blog.html?post=${nextPost.year}-${nextPost.month}-${nextPost.day}${showEncryptedParam === "true" ? "&e=true" : ""}`;
     
-    title.innerHTML = `${json.posts[postIndex].date} - ${json.posts[postIndex].title}`;
-    if (json.posts[postIndex].subtitle) {
-      subtitle.innerHTML = json.posts[postIndex].subtitle;
+    title.innerHTML = `${posts[postIndex].date} - ${posts[postIndex].title}`;
+    if (posts[postIndex].subtitle) {
+      subtitle.innerHTML = posts[postIndex].subtitle;
       subtitle.removeAttribute("hidden");
     } else {
       subtitle.setAttribute("hidden", "");
     }
 
-    if (json.posts[postIndex].encrypted) {
+    if (posts[postIndex].encrypted) {
       decryptor.style.display = "block";
     } else {
       decryptor.style.display = "none";
     }
     
-    content.innerHTML = json.posts[postIndex].content;
+    content.innerHTML = posts[postIndex].content;
     return json;
   });
 
 function decrypt() {
   postsJson.then((json) => {
     const queryParams = new URLSearchParams(window.location.search);
-    const postParam = queryParams.get("post");
-    const blogPost = postParam !== null && !isNaN(parseInt(postParam)) ? parseInt(postParam) : json.posts.length - 1;
-    const postIndex = json.posts.length - 1 - blogPost;
+    const postParam = queryParams.get("post")?.split("-");
+    const [year, month, day] = postParam?.length === 3 ? postParam.map((it) => parseInt(it)) : [null, null, null];
+    const postIndex = postParam?.length === 3 ? json.posts.findIndex((it) => it.year === year && it.month === month && it.day === day) : posts.length - 1;
 
     content.innerHTML = decipher(json.posts[postIndex].content, key.value);
   })
